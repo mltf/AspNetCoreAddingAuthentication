f5e35dd1c06de0307f719a03936c93e29e54aaa5
Rajesh Kalaskar <Rajesh.Kalaskar@trader.ca>
2018-11-28 09:59:56 -0500

    Add Authentication Support

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WishList.Data;
+using WishList.Models;
 
 namespace WishList
 {
@@ -14,6 +16,12 @@ namespace WishList
         {
             services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Wishlist"));
+            // AddIdentity: Adds the default identity system configuration for the specified User and Role types.
+            // AddEntityFrameworkStores: Adds an Entity Framework implementation of identity information stores.
+            // AddDefaultTokenProviders: Adds the default token providers used to generate tokens for reset passwords, change email and change telephone number operations, and for two factor authentication token generation.
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +35,9 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            // UseAuthentication: Adds the <see cref="T:Microsoft.AspNetCore.Authentication.AuthenticationMiddleware" /> to the specified <see cref="T:Microsoft.AspNetCore.Builder.IApplicationBuilder" />, which enables authentication capabilities.
+            app.UseAuthentication();
 
             app.UseMvcWithDefaultRoute();
         }
